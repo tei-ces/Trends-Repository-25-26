918f35f0afa559f7c77ee04d239ed13315576167
Althea Nicole S. Cestina <ancestina@gbox.adnu.edu.ph>
2026-05-13 21:17:43 -0500

    Add files via upload

---
+++ cestinazs21am/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Member
+
+def index(request):
+    obj=Member.objects.all()
+    context={
+        "obj":obj,
+    }
+    return render(request, "index.html",context)
